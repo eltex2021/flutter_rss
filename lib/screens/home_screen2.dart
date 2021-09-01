0ea244338c2e626fcb2e6cca7d213841b4d061cb
Sergey Kolpakov <eltexnika2008@yandex.ru>
2021-09-01 15:45:58 +0500

    несколько изменений

--- lib/screens/home_screen2.dart
+++ lib/screens/home_screen2.dart
@@ -4,98 +4,98 @@ import 'dart:io' as io;
 import 'package:xml_parser/xml_parser.dart';
 
 
-// пробую парсить xml!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//пробую парсить xml!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-// class HomeScreenRSS2 extends StatefulWidget {
-//   @override
-//   _HomeScreenRSSState createState() => _HomeScreenRSSState();
-// }
-//
-// class _HomeScreenRSSState extends State {
-//   bool _darkTheme = false;
-//   List _productsList = [];
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       theme: !_darkTheme ? ThemeData.light() : ThemeData.dark(),
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text("Парсер"),
-//           actions: [
-//             Icon(_getAppBarIcon()),
-//             _getPlatformSwitch(),
-//           ],
-//         ),
-//         body: FutureBuilder(
-//           future: _getHttp(),
-//           builder: (context, AsyncSnapshot snapshot) {
-//             if (!snapshot.hasData) {
-//               return Center(
-//                 child: CircularProgressIndicator(),
-//               );
-//             } else {
-//               return Container(
-//                 child: ListView.builder(
-//                     scrollDirection: Axis.vertical,
-//                     itemCount: _productsList.length,
-//                     itemBuilder: (BuildContext context, int index) {
-//                       return Card(
-//                         child: Container(
-//                           child: Column(
-//                             //  children:[Text('привет')],
-//                             children: [Text('${_productsList[index].title}')],
-//                           ),
-//                         ),
-//                       );
-//                     }),
-//               );
-//             }
-//           },
-//         ),
-//       ),
-//     );
-//   }
-//
-//   _getAppBarIcon() {
-//     if (_darkTheme) {
-//       return Icons.lightbulb_outline;
-//     } else {
-//       return Icons.highlight;
-//     }
-//   }
-//
-//   _getPlatformSwitch() {
-//     if (io.Platform.isIOS) {
-//       return CupertinoSwitch(
-//         value: _darkTheme,
-//         onChanged: (bool value) {
-//           setState(() {
-//             _darkTheme = !_darkTheme;
-//           });
-//         },
-//       );
-//     } else if (io.Platform.isAndroid) {
-//       return Switch(
-//         value: _darkTheme,
-//         onChanged: (bool value) {
-//           setState(() {
-//             _darkTheme = !_darkTheme;
-//           });
-//         },
-//       );
-//     }
-//   }
-//
-//
-//
-//
-//   _getHttp() async {
-//     XmlDocument xmlDocument =
-//     await XmlDocument.fromUri(
-//         'https://xn----btbtc0cdch1fua.xn--p1ai/index.php?route=extension/feed/ocext_feed_generator_google&token=2375316');
-//
-//     return _productsList;
-//     }
-// }
+class HomeScreenRSS2 extends StatefulWidget {
+  @override
+  _HomeScreenRSSState createState() => _HomeScreenRSSState();
+}
+
+class _HomeScreenRSSState extends State {
+  bool _darkTheme = false;
+  List _productsList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: !_darkTheme ? ThemeData.light() : ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Парсер"),
+          actions: [
+            Icon(_getAppBarIcon()),
+            _getPlatformSwitch(),
+          ],
+        ),
+        body: FutureBuilder(
+          future: _getHttp(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return Container(
+                child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: _productsList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        child: Container(
+                          child: Column(
+                            //  children:[Text('привет')],
+                            children: [Text('${_productsList[index].title}')],
+                          ),
+                        ),
+                      );
+                    }),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  _getAppBarIcon() {
+    if (_darkTheme) {
+      return Icons.lightbulb_outline;
+    } else {
+      return Icons.highlight;
+    }
+  }
+
+  _getPlatformSwitch() {
+    if (io.Platform.isIOS) {
+      return CupertinoSwitch(
+        value: _darkTheme,
+        onChanged: (bool value) {
+          setState(() {
+            _darkTheme = !_darkTheme;
+          });
+        },
+      );
+    } else if (io.Platform.isAndroid) {
+      return Switch(
+        value: _darkTheme,
+        onChanged: (bool value) {
+          setState(() {
+            _darkTheme = !_darkTheme;
+          });
+        },
+      );
+    }
+  }
+
+
+
+
+  _getHttp() async {
+    XmlDocument xmlDocument =
+    await XmlDocument.fromUri(
+        'https://xn----btbtc0cdch1fua.xn-');
+
+    return _productsList;
+    }
+}
